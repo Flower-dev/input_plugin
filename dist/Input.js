3b888e0b173774dd9d8e6b57c5e3b5eb6e1aac70
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-06-07 22:16:19 +0200

    plugin create

---
+++ dist/Input.js
@@ -0,0 +1,56 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Input;
+
+require("core-js/modules/web.dom-collections.iterator.js");
+
+var _react = require("react");
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+require("./input.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// -----------------------------------
+Input.propTypes = {
+  type: _propTypes.default.string,
+  name: _propTypes.default.string,
+  placeholder: _propTypes.default.string,
+  onChange: _propTypes.default.func,
+  required: _propTypes.default.bool,
+  min: _propTypes.default.string
+};
+Input.defaultProps = {
+  required: false,
+  min: '0'
+}; // ------------------------------------
+
+function Input(_ref) {
+  let {
+    type,
+    name,
+    placeholder,
+    onChange: _onChange,
+    required,
+    min
+  } = _ref;
+  const [initialValue, setInitialValue] = (0, _react.useState)('');
+  return /*#__PURE__*/React.createElement("input", {
+    className: "inputForm",
+    type: type,
+    name: name,
+    required: required,
+    placeholder: placeholder,
+    min: min,
+    value: initialValue,
+    onChange: e => {
+      setInitialValue(e.target.value);
+
+      _onChange(e.target.value);
+    }
+  });
+}
---
+++ src/lib/Input.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './input.css';
+
+// -----------------------------------
+
+Input.propTypes = {
+    type: PropTypes.string,
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    required: PropTypes.bool,
+    min: PropTypes.string
+}
+
+Input.defaultProps = {
+    required: false,
+    min: '0'
+}
+
+// ------------------------------------
+
+
+export default function Input({ type, name, placeholder, onChange, required, min }) {
+    
+    const [initialValue, setInitialValue] = useState('');
+
+    return  (
+        <input 
+            className='inputForm' 
+            type={type}
+            name={name}
+            required={required}
+            placeholder={placeholder}
+            min={min} 
+            value={initialValue}   
+            onChange={(e) => {
+                setInitialValue(e.target.value)
+                onChange(e.target.value)
+            }} 
+        />
+    )
+}
